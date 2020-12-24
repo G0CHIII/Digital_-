ac2c394f312ca7adffe8c2d67b067d23eb5c0c0f
G0CHIII <75804911+G0CHIII@users.noreply.github.com>
2020-12-24 21:53:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.image import Image
+from PIL import Image
+from kivy.uix.scrollview import ScrollView
+import webbrowser
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.properties import StringProperty
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+from kivy.uix.textinput import TextInput
+import random
+from kivy.uix.boxlayout import BoxLayout
+
+from io import open
+from kivy.config import Config
+
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+
+Window.size = (480, 853)
+
+#with open("illust.kv", encoding='utf-8') as f:
+#    drunk = Builder.load_string(f.read())
+
+watchlist = ['Медный всадник А.С. Пушкин: Иллюстрации',
+                 'Медный всадник А.С. Пушкин: Спектакль']
+readlist = ['Медный всадник А.С. Пушкин: О книге',
+                'Медный всадник А.С. Пушкин: О наводнении',
+                'Медный всадник А.С. Пушкин: О печати на книге']
+listenlist = ['Медный всадник А.С. Пушкин: Аудиокнига']
+
+choisesscreen = {'Медный всадник А.С. Пушкин: Иллюстрации': 'illust',
+                     'Медный всадник А.С. Пушкин: Спектакль': 'vidbook',
+                     'Медный всадник А.С. Пушкин: О книге': 'abbook',
+                     'Медный всадник А.С. Пушкин: О наводнении': 'voda',
+                     'Медный всадник А.С. Пушкин: О печати на книге': 'sign',
+                     'Медный всадник А.С. Пушкин: Аудиокнига': 'audbook'}
+
+
+class Search(TextInput):
+    choicesfile = StringProperty()
+
+    def __init__(self, **kwargs):
+        self.choicesfile = kwargs.pop('choicesfile', '')
+        self.choiceslist = ['Медный всадник А.С. Пушкин: Иллюстрации',
+                            'Медный всадник А.С. Пушкин: Спектакль',
+                            'Медный всадник А.С. Пушкин: О книге',
+                            'Медный всадник А.С. Пушкин: О наводнении',
+                            'Медный всадник А.С. Пушкин: О печати на книге',
+                            'Медный всадник А.С. Пушкин: Аудиокнига']
+        self.choisesscreen = {'Медный всадник А.С. Пушкин: Иллюстрации': 'illust',
+                              'Медный всадник А.С. Пушкин: Спектакль': 'vidbook',
+                              'Медный всадник А.С. Пушкин: О книге': 'abbook',
+                              'Медный всадник А.С. Пушкин: О наводнении': 'voda',
+                              'Медный всадник А.С. Пушкин: О печати на книге': 'sign',
+                              'Медный всадник А.С. Пушкин: Аудиокнига': 'audbook'}
+        super(Search, self).__init__(**kwargs)
+        self.bind(choicesfile=self.load_choices)
+        self.bind(text=self.on_text)
+        self.load_choices()
+        self.dropdown = None
+        self.hint_text = 'Поиск'
+        self.font_size = 20
+
+    def load_choices(self):
+        if self.choicesfile:
+            with open(self.choicesfile) as fd:
+                for line in fd:
+                    self.choiceslist.append(line.strip('\n'))
+        self.values = []
+
+    def on_text(self, Search, text):
+        if self.dropdown:
+            self.dropdown.dismiss()
+            self.dropdown = None
+        if text == '':
+            return
+        values = []
+        for addr in self.choiceslist:
+            if text.lower() in addr.lower():
+                values.append(addr)
+        self.values = values
+        if len(values) > 0:
+            self.dropdown = DropDown()
+            for val in self.values[:10]:
+                self.dropdown.add_widget(Button(text=val, size_hint_y=None, height=48, font_size=15, halign='left',
+                                                background_color=[255, 255, 255, 1], color=[0, 0, 0, 1],
+                                                on_release=self.do_choose))
+            self.dropdown.add_widget(Button(size_hint_y=None, height=1000, background_color=[255, 255, 255, 0.9]))
+            self.dropdown.open(self)
+
+    def do_choose(self, butt):
+        screen_to_go = self.choisesscreen[butt.text]
+        if self.dropdown:
+            App.get_running_app().sm.current = screen_to_go
+            self.dropdown.dismiss()
+            self.dropdown = None
+
+watch_random = random.choice(watchlist)
+listen_random = random.choice(listenlist)
+read_random = random.choice(readlist)
+
+class MainScreenManager(ScreenManager):
+    watch_screen = StringProperty(choisesscreen[watch_random])
+    listen_screen = StringProperty(choisesscreen[listen_random])
+    read_screen = StringProperty(choisesscreen[read_random])
+    pass
+
+class MyApp(App):
+    watch = StringProperty(watch_random)
+    listen = StringProperty(listen_random)
+    read = StringProperty(read_random)
+    def build(self):
+        self.sm = MainScreenManager()
+        return self.sm
+
+
+
+if __name__ == '__main__':
+    MyApp().run()
